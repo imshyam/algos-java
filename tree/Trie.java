4c4748a49858fec109d63261fef3b62226db79c1
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2019-05-06 02:26:00 +0800

    trie incomplete

---
+++ tree/Trie.java
@@ -0,0 +1,87 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Trie {
+
+  class TrieNode {
+    Map<Character, TrieNode> children;
+    boolean endOfWorld;
+
+    TrieNode() {
+      children = new HashMap<>();
+      endOfWorld = false;
+    }
+  }
+
+  private final TrieNode root;
+  Trie() {
+    root = new TrieNode();
+  }
+
+  void insert(String str) {
+    TrieNode current = root;
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      TrieNode node = current.children.get(ch);
+      if (node == null) {
+        node = new TrieNode();
+        node.children.put(ch, node);
+      }
+      current = node;
+    }
+    current.endOfWorld = false;
+  }
+
+  boolean search(String word) {
+    TrieNode current = root;
+    for (int i = 0; i < word.length(); i++) {
+      char ch = word.charAt(i);
+      TrieNode node = current.children.get(ch);
+      if (node == null) {
+        return false;
+      }
+      current = node;
+    }
+    return current.endOfWorld;
+  }
+
+  void delete(TrieNode root, String word) {
+    delete(root, word, 0);
+  }
+
+  boolean delete(TrieNode current, String word, int index) {
+    if (index == word.length()) {
+      if (!current.endOfWorld) {
+        return false;
+      }
+      current.endOfWorld = false;
+      return current.children.size() == 0;
+    }
+    char ch = word.charAt(index);
+    TrieNode node = current.children.get(ch);
+    if (node == null) {
+      return false;
+    }
+    boolean shouldDeleteCurrentNode = delete(current, word, index);
+    if (shouldDeleteCurrentNode) {
+      current.children.remove(ch);
+      return current.children.size() == 0;
+    }
+    return false;
+  }
+
+}
+
+class TrieTest {
+  public static void main(String[] args) {
+    Trie trie = new Trie();
+    trie.insert("apple");
+    trie.insert("book");
+    trie.insert("cat");
+    trie.insert("adam");
+    trie.insert("axe");
+    trie.insert("amy");
+    System.out.println(trie.search("amy"));
+    System.out.println(trie.search("axe"));
+  }
+}
